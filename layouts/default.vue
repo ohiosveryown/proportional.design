2ce0b207b9790d7c4a582dec1f31edb534bb071c
ohiosveryown <matt@ohiosveryown.co>
2021-12-29 06:56:53 +0000

    Added styling to html for overflow scroll

--- layouts/default.vue
+++ layouts/default.vue
@@ -16,6 +16,7 @@
         const html = document.documentElement
         if (this.currentTime >= 20 && this.currentTime <= 24) {
           console.log("it's evening 🌙")
+          html.style.background = "#080E2D"
           body.style.cssText = `
             background: url(https://res.cloudinary.com/da32ufmnf/image/upload/v1640758491/proportional.design-v2/bg/evening_wbgosa.svg) no-repeat top center fixed;
             background-size: cover;
@@ -23,13 +24,15 @@
           `
         } else if (this.currentTime >= 0 && this.currentTime < 7) {
           console.log("it's still night 🌗")
+          html.style.background = "#03081D"
           body.style.cssText = `
               background: url(https://res.cloudinary.com/da32ufmnf/image/upload/v1640758898/proportional.design-v2/bg/night_ig0bgl.svg) no-repeat top center fixed;
               background-size: cover;
               color: #fff;
-            `
+          `
         } else if (this.currentTime >= 7 && this.currentTime < 12) {
           console.log("it's morning time ⛅")
+          html.style.background = "#A8D4F5"
           body.style.cssText = `
               background: url(https://res.cloudinary.com/da32ufmnf/image/upload/v1640758051/proportional.design-v2/bg/morning_j4tsrx.svg) no-repeat top center fixed;
               background-size: cover;
@@ -37,13 +40,15 @@
             `
         } else if (this.currentTime >= 12 && this.currentTime < 17) {
           console.log("it's the afternoon ☀️")
+          html.style.background = "#547BD5"
           body.style.cssText = `
-            background: url(https://res.cloudinary.com/da32ufmnf/image/upload/v1640759377/proportional.design-v2/bg/afternoon_qliyuv.svg) no-repeat top center fixed;
+            background: url(https://res.cloudinary.com/da32ufmnf/image/upload/v1640760670/proportional.design-v2/bg/afternoon_zcddoy.svg) no-repeat top center fixed;
             background-size: cover;
             color: #fff;
           `
         } else {
           console.log("it's dusk 🌇")
+          html.style.background = "#060210"
           body.style.cssText = `
             background: url(https://res.cloudinary.com/da32ufmnf/image/upload/v1640757207/proportional.design-v2/bg/dusk_q3xtfh.svg) no-repeat top center fixed;
             background-size: cover;
