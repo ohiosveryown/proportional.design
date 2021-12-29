06182d76f2d31cd1fc5f0f52c0c0f30f767fb5d2
ohiosveryown <matt@ohiosveryown.co>
2021-12-29 04:52:18 +0000

    Project detail looking good

--- content/projects/swan.md
+++ content/projects/swan.md
@@ -1,7 +1,19 @@
 ---
 hero: https://res.cloudinary.com/da32ufmnf/image/upload/v1640651657/proportional.design-v2/roop_l4jj0j.jpg
 title: Swan
+title_full: Swan Breakfast Table
+tags:
+  tag_one: oak
+  tag_two: table
 date: 2021-12-27T20:51:31.542Z
 ---
 
-## Swan Breakfast Table
+As I've been building furniture more seriously for the past two and a half years, I've wanted to document builds and progress, if for nothing else than a repository for me to revisit – and maybe even offer some insight into how I build things.
+
+The first iteration of proportional.design had some limitations, the biggest being it was a pain to update and wasn't very blog-like.
+
+![Hiro Chair](https://res.cloudinary.com/da32ufmnf/image/upload/v1612969573/proportional.design/social-00_o80ipz.jpg)
+
+As I've been building furniture more seriously for the past two and a half years, I've wanted to document builds and progress, if for nothing else than a repository for me to revisit – and maybe even offer some insight into how I build things.
+
+The first iteration of proportional.design had some limitations, the biggest being it was a pain to update and wasn't very blog-like.
--- pages/projects/_slug.vue
+++ pages/projects/_slug.vue
@@ -1,8 +1,81 @@
 <template>
-  <div>
-    <nuxt-content :document="article" />
+  <div class="wrapper">
+    <main class="">
+      <a href="" @click.prevent="$router.back()">
+        <button class="back">
+          <svg width="14" height="14" fill="none">
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M16.03 6.098H4.185l4.402-4.447a.974.974 0 0 0 0-1.368.95.95 0 0 0-1.353 0L1.276 6.301a.975.975 0 0 0-.135 1.196.966.966 0 0 0 .174.24l5.92 5.98a.95.95 0 0 0 1.353 0 .974.974 0 0 0 0-1.367L4.313 8.032H16.03a.967.967 0 0 0 0-1.934Z"
+              fill="#fff"
+            />
+          </svg>
+        </button>
+      </a>
+
+      <h1 class="title neuebit">{{ article.title_full }}</h1>
+
+      <ul if="article.tags" class="tags">
+        <li v-if="article.tags.tag_one">#{{ article.tags.tag_one }}</li>
+        <li v-if="article.tags.tag_two">#{{ article.tags.tag_two }}</li>
+        <li v-if="article.tags.tag_three">#{{ article.tags.tag_three }}</li>
+      </ul>
+
+      <div class="images">
+        <img class="project-hero" :src="`${article.hero}`" />
+      </div>
+
+      <article class="p-timeline">
+        <nuxt-content :document="article" />
+      </article>
+    </main>
+
+    <div class="divider-wrapper">
+      <div class="divider" />
+    </div>
+
+    <aside class="">
+      <div class="cards">
+        <div class="card card--contact">
+          <header>Get in Touch</header>
+          <p>
+            Interested in a custom project or want to learn more? Smash that
+            contact button below.
+          </p>
+          <contact class="primary" />
+        </div>
+
+        <section class="card card--rating">
+          <div class="question">
+            <header>Care to rate this project?</header>
+            <p>Ratings help me build more things people like</p>
+            <ul class="emojis">
+              <li class="emoji">🔥</li>
+              <li class="emoji">❤️</li>
+              <li class="emoji">🤔</li>
+              <li class="emoji">👎</li>
+            </ul>
+          </div>
+          <div class="answer">
+            <header>Hey, Thanks</header>
+            <p>I really appreciate the feedback 🙌.</p>
+          </div>
+        </section>
+
+        <div class="card card--next-post">
+          <header>More From the Timeline</header>
+          <p>More posts go here</p>
+        </div>
+      </div>
+    </aside>
   </div>
 </template>
+
+<style lang="scss" scoped>
+  @import "~static/style/grid.scss";
+  @import "~static/style/detail.scss";
+</style>
 
 <script>
   export default {
@@ -14,6 +87,39 @@
         error({ message: "Projects not found" })
       }
       return { article }
+    },
+    methods: {
+      formatDate(date) {
+        const options = { year: "numeric", month: "long", day: "numeric" }
+        return new Date(date).toLocaleDateString("en", options)
+      },
+      formatTime(time) {
+        return new Date(time).toLocaleTimeString("en", { timeStyle: "short" })
+      },
+      escape() {
+        document.addEventListener("keydown", () => {
+          if (event.key === "Escape") {
+            this.$nuxt.$options.router.push("/")
+          }
+        })
+      },
+    },
+    mounted() {
+      this.escape()
+      const emojis = document.querySelectorAll(".emoji")
+      const question = document.querySelector(".question")
+      const answer = document.querySelector(".answer")
+      emojis.forEach((emoji) => {
+        emoji.addEventListener("click", () => {
+          question.style.display = "none"
+          answer.style.display = "flex"
+          answer.style.cssText = `
+            display: inherit;
+            visibility: visible;
+            opacity: 1;
+          `
+        })
+      })
     },
   }
 </script>
