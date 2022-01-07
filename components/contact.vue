8e948d5fabfcc0f59a825c059f71a6334d7cdaaf
ohiosveryown <matt@ohiosveryown.co>
2022-01-07 18:53:31 +0000

    began contact form

--- components/contact.vue
+++ components/contact.vue
@@ -1,5 +1,30 @@
 <template>
-  <button class="neuebit uc">Contact</button>
+  <div>
+    <button class="neuebit uc">Contact</button>
+
+    <form
+      name="contactus"
+      action="/thanks"
+      method="post"
+      netlify
+      netlify-honeypot="bot-field"
+    >
+      <input type="hidden" name="form-name" value="contactus" />
+      <div>
+        <label for="name">Name:</label>
+        <input type="text" name="name" required />
+      </div>
+      <div>
+        <input type="email" name="email" required />
+        <label for="email">Email:</label>
+      </div>
+      <div>
+        <textarea name="message" required></textarea>
+        <label for="message">Message:</label>
+      </div>
+      <button type="submit" value="Send message">Send</button>
+    </form>
+  </div>
 </template>
 
 <style lang="scss" scoped>
