af91a669b5e5cf2404c950f2b83a308fad51bb9b
ohiosveryown <matt@ohiosveryown.co>
2024-12-23 11:46:07 -0500

    b4 "Controls" refactor

---
+++ components/Controls.vue
@@ -0,0 +1,12 @@
+<template>
+  <menu>
+    <div>Sorting</div>
+    <div>Filters</div>
+  </menu>
+</template>
+
+<style lang="scss" scoped>
+@use "/assets/style/grid.scss" as *;
+</style>
+
+<script setup></script>
