e9ba247fc8c5b24ef29fb87d5717f4e72ffdca8f
ohiosveryown <matt@ohiosveryown.co>
2025-01-13 18:12:02 -0500

    update variables

--- composables/firebase.ts
+++ composables/firebase.ts
@@ -2,14 +2,16 @@
 import { initializeApp } from 'firebase/app'
 import { getDatabase } from 'firebase/database'
 
+const config = useRuntimeConfig()
+
 const firebaseConfig = {
-  apiKey: "AIzaSyD6dC_tdonSgsGcjPOohyEsBB2pxLOCuuI",
-  authDomain: "likecounter-c8b3f.firebaseapp.com",
-  databaseURL: "https://likecounter-c8b3f-default-rtdb.firebaseio.com",
-  projectId: "likecounter-c8b3f",
-  storageBucket: "likecounter-c8b3f.firebasestorage.app",
-  messagingSenderId: "14243289582",
-  appId: "1:14243289582:web:d3b69a7839b557b46c16ff"
+  apiKey: config.public.FIREBASE_API_KEY,
+  authDomain: config.public.FIREBASE_AUTH_DOMAIN,
+  databaseURL: config.public.FIREBASE_DATABASE_URL,
+  projectId: config.public.FIREBASE_PROJECT_ID,
+  storageBucket: config.public.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: config.public.FIREBASE_MESSAGING_SENDER_ID,
+  appId: config.public.FIREBASE_APP_ID
 }
 
 export const useFirebase = () => {
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,4 +19,15 @@ export default defineNuxtConfig({
     host: '0.0.0.0',
     port: 3000
   },
+  runtimeConfig: {
+    public: {
+      FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+      FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+      FIREBASE_DATABASE_URL: process.env.FIREBASE_DATABASE_URL,
+      FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+      FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+      FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      FIREBASE_APP_ID: process.env.FIREBASE_APP_ID
+    }
+  }
 })
